d0710ab2b77003eae4d7b7d9070ad7411d9c452b
崔嘉俊 <1394446290@qq.com>
2019-04-23 15:47:54 +0800

    发标前审核

--- core/src/main/java/study/cjj/eloan/base/domain/BidRequest.java
+++ core/src/main/java/study/cjj/eloan/base/domain/BidRequest.java
@@ -30,17 +30,23 @@ public class BidRequest extends BaseDomain{
 	private int bidRequestState = BidConst.BIDREQUEST_STATE_PUBLISH_PENDING; // 这个标的状态
 
 	private BigDecimal bidRequestAmount = BidConst.ZERO; // 借款金额
+	
 	private BigDecimal currentRate = BidConst.ZERO; // 借款利率
-	private BigDecimal minBidAmount = BidConst.SMALLEST_BID_AMOUNT;// 最小投标
 	private int monthes2Return = 1; // 借款期限(月份数1~12)
+	
 	private int bidCount = 0; // 已有投标数量
 	private BigDecimal totalRewardAmount = BidConst.ZERO; // 总报酬金额
+	
+	private BigDecimal minBidAmount = BidConst.SMALLEST_BID_AMOUNT;// 最小投标
 	private BigDecimal currentSum = BidConst.ZERO; // 当前已经借到多少钱
+	
 	private String title = ""; // 借款标题
 	private String description = ""; // 借款描述
 	private String note = ""; // 风控评审意见
+	
 	private Date disableDate = new Date(); // 招标到期时间 (页面数据是招标天数，无效日期=招标天数+当前日期)
 	private int disableDays = 0; // 标的有效天数
+	
 	private LoginInfo createUser; // 借款人
 	private List<Bid> bids = new ArrayList<Bid>(); // 这个借款已经有的标(已经收到的投标)
 	private Date applyTime;// 申请时间
@@ -49,6 +55,35 @@ public class BidRequest extends BaseDomain{
 	public String getReturnTypeDisplay() {
 		return returnType == BidConst.RETURN_TYPE_MONTH_INTEREST ? "按月到期"
 				: "等额本息";
+	}
+	
+	public String getBidRequestStateDisplay() {
+		switch (this.bidRequestState) {
+		case BidConst.BIDREQUEST_STATE_PUBLISH_PENDING:
+			return "待发布";
+		case BidConst.BIDREQUEST_STATE_BIDDING:
+			return "招标中";
+		case BidConst.BIDREQUEST_STATE_UNDO:
+			return "已撤销";
+		case BidConst.BIDREQUEST_STATE_BIDDING_OVERDUE:
+			return "流标";
+		case BidConst.BIDREQUEST_STATE_APPROVE_PENDING_1:
+			return "满标一审";
+		case BidConst.BIDREQUEST_STATE_APPROVE_PENDING_2:
+			return "满标二审";
+		case BidConst.BIDREQUEST_STATE_REJECTED:
+			return "满标审核被拒";
+		case BidConst.BIDREQUEST_STATE_PAYING_BACK:
+			return "还款中";
+		case BidConst.BIDREQUEST_STATE_COMPLETE_PAY_BACK:
+			return "完成";
+		case BidConst.BIDREQUEST_STATE_PAY_BACK_OVERDUE:
+			return "逾期";
+		case BidConst.BIDREQUEST_STATE_PUBLISH_REFUSE:
+			return "发标拒绝";
+		default:
+			return "";
+		}
 	}
 	
 	public String getJsonString(){
--- core/src/main/java/study/cjj/eloan/base/query/BidRequestQueryObject.java
+++ core/src/main/java/study/cjj/eloan/base/query/BidRequestQueryObject.java
@@ -10,5 +10,8 @@ public class BidRequestQueryObject extends BaseAuditQueryObject{
 	private int bidRequestState = -1;//标的的状态
 	private Long creatorId;//标的申请人
 	private int[] states;//按照多种状态查询标的
+	
+	private String orderBy ;  //按照哪个列排序 
+	private String orderType;  //按照什么顺序配列
 
 }
